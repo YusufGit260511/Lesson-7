d127082bc3043d3c18790a6ab37b1e65a428173f
YusufGit260511 <yusufgithub2605@gmail.com>
2025-04-19 00:17:36 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,76 @@
+var typed = new Typed('#firstExercise_h1', {
+    strings: ['Text Gradients', ''], // Массив строк, которые будут печататься
+    typeSpeed: 80, // Скорость набора текста (мс на символ)
+    backSpeed: 40, // Скорость удаления текста (мс на символ)
+    // startDelay: 1000, // Задержка перед началом анимации (в мс)
+    backDelay: 3700, // Задержка перед удалением текста (в мс)
+    loop: true, // Повторять ли анимацию бесконечно
+    // smartBackspace: true, // Удалять только разные символы между строками
+    // shuffle: false, // Если true, строки будут выводиться в случайном порядке
+    fadeOut: false, // Если true — текст будет исчезать вместо удаления
+    // fadeOutClass: 'typed-fade-out', // Класс для эффекта исчезновения (если fadeOut = true)
+    // fadeOutDelay: 500, // Задержка перед исчезновением (если fadeOut = true)
+    showCursor: false, // Показывать ли курсор
+    // cursorChar: '|', // Символ курсора
+    // autoInsertCss: true, // Автоматически добавлять стили (нужно для эффекта печати и курсора)
+    // bindEventFocusEvents: false, // Следить ли за событиями фокуса (обычно для input)
+    // attr: null, // Если указать атрибут (например, "placeholder") — текст будет печататься туда, а не в элемент
+    // contentType: 'html', // Тип содержимого: 'html' — можно использовать теги, 'null' — просто текст
+});
+var typed2 = new Typed('#firstExercise_p', {
+    strings: ['Make your headlines stand out', ''], // Массив строк, которые будут печататься
+    typeSpeed: 80, // Скорость набора текста (мс на символ)
+    backSpeed: 40, // Скорость удаления текста (мс на символ)
+    startDelay: 1700, // Задержка перед началом анимации (в мс)
+    backDelay: 1500, // Задержка перед удалением текста (в мс)
+    loop: true, // Повторять ли анимацию бесконечно
+    // smartBackspace: true, // Удалять только разные символы между строками
+    // shuffle: false, // Если true, строки будут выводиться в случайном порядке
+    fadeOut: false, // Если true — текст будет исчезать вместо удаления
+    // fadeOutClass: 'typed-fade-out', // Класс для эффекта исчезновения (если fadeOut = true)
+    // fadeOutDelay: 500, // Задержка перед исчезновением (если fadeOut = true)
+    showCursor: false, // Показывать ли курсор
+    // cursorChar: '|', // Символ курсора
+    // autoInsertCss: true, // Автоматически добавлять стили (нужно для эффекта печати и курсора)
+    // bindEventFocusEvents: false, // Следить ли за событиями фокуса (обычно для input)
+    // attr: null, // Если указать атрибут (например, "placeholder") — текст будет печататься туда, а не в элемент
+    // contentType: 'html', // Тип содержимого: 'html' — можно использовать теги, 'null' — просто текст
+});
+var typed3 = new Typed('#secondh1', {
+    strings: ['Design is intelligence <br> made visible', ''], // Массив строк, которые будут печататься
+    typeSpeed: 80, // Скорость набора текста (мс на символ)
+    backSpeed: 40, // Скорость удаления текста (мс на символ)
+    startDelay: 1700, // Задержка перед началом анимации (в мс)
+    backDelay: 1500, // Задержка перед удалением текста (в мс)
+    loop: true, // Повторять ли анимацию бесконечно
+    // smartBackspace: true, // Удалять только разные символы между строками
+    // shuffle: false, // Если true, строки будут выводиться в случайном порядке
+    fadeOut: false, // Если true — текст будет исчезать вместо удаления
+    // fadeOutClass: 'typed-fade-out', // Класс для эффекта исчезновения (если fadeOut = true)
+    // fadeOutDelay: 500, // Задержка перед исчезновением (если fadeOut = true)
+    showCursor: false, // Показывать ли курсор
+    // cursorChar: '|', // Символ курсора
+    // autoInsertCss: true, // Автоматически добавлять стили (нужно для эффекта печати и курсора)
+    // bindEventFocusEvents: false, // Следить ли за событиями фокуса (обычно для input)
+    // attr: null, // Если указать атрибут (например, "placeholder") — текст будет печататься туда, а не в элемент
+    // contentType: 'html', // Тип содержимого: 'html' — можно использовать теги, 'null' — просто текст
+});
+var typed4 = new Typed('#secondp', {
+    strings: ['Interaction design isn’t merely a matter of aesthetic choice; rather, <br> it is based on an understanding of users and cognitive principles.', ''], // Массив строк, которые будут печататься
+    typeSpeed: 80, // Скорость набора текста (мс на символ)
+    backSpeed: 40, // Скорость удаления текста (мс на символ)
+    startDelay: 1700, // Задержка перед началом анимации (в мс)
+    backDelay: 1500, // Задержка перед удалением текста (в мс)
+    loop: true, // Повторять ли анимацию бесконечно
+    // smartBackspace: true, // Удалять только разные символы между строками
+    // shuffle: false, // Если true, строки будут выводиться в случайном порядке
+    fadeOut: false, // Если true — текст будет исчезать вместо удаления
+    // fadeOutClass: 'typed-fade-out', // Класс для эффекта исчезновения (если fadeOut = true)
+    // fadeOutDelay: 500, // Задержка перед исчезновением (если fadeOut = true)
+    showCursor: false, // Показывать ли курсор
+    // cursorChar: '|', // Символ курсора
+    // autoInsertCss: true, // Автоматически добавлять стили (нужно для эффекта печати и курсора)
+    // bindEventFocusEvents: false, // Следить ли за событиями фокуса (обычно для input)
+    // attr: null, // Если указать атрибут (например, "placeholder") — текст будет печататься туда, а не в элемент
+    // contentType: 'html', // Тип содержимого: 'html' — можно использовать теги, 'null' — просто текст
+});
